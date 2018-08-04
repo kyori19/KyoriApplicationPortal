13941158a79f2fd32921cdacf26c37ff83f08cec
kyori <kyori.dis@gmail.com>
2018-08-04 14:49:06 +0900

    indexの改修,ページ追加(中身はない)

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ def main():
     title = "Kyori Application Portal"
     return render_template("index.html", title=title)
 
+@app.route("/regulusaurum", methods=["GET"])
+def regulusaurum():
+    title = "EasyTootSaver ~獅子の黄金~"
+    return render_template("regulusaurum.html", title=title)
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    title = "About きょり/わんせた"
+    return render_template("profile.html", title=title)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=1443)
